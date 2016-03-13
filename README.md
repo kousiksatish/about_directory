4405efc1610e08d15ab3593e0d1d3a7c79e13870
Kousik Satish <kousiksatish@gmail.com>
2016-03-13 16:49:45 +0530

    setup, readme updated

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-##Build Instructions
-1. Clone the repository and move into that directory
-2. Create a virtual environment by `virtualenv env`
-3. Activate the virtual environment `source env/bin/activate`
-4. Run `pip install -r requirements.txt` to install requirements
+For installation 
+
+`python setup.py install`
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup (
 	name= "aboutdir",
 	version = "1.0",
 	description = 'Command line tool for adding description to directories',
-	url = '',
+	url = 'https://github.com/kousiksatish/about_directory',
 	author = 'Kousik Satish',
 	author_email = 'kousiksatish@gmail.com',
 
@@ -12,6 +12,7 @@ setup (
     entry_points = {
     	'console_scripts' : ['aboutdir=aboutdir:main']
     },
+    keywords=['CLI', 'directory', 'aboutdir', 'about-directory', 'description-for-directory'],
     classifiers=[
 	    'Programming Language :: Python :: 2',
 	    'Programming Language :: Python :: 2.6',
